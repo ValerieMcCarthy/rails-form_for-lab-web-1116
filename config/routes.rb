5f181e0ebf86169ed3cd2cc40e66fea2b19bef32
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-12-27 22:24:06 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :students, :school_classes
+  
+  #get 'students/index'
+
+  #get 'school_classes/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
